b1e5e8ed176a99a9ebd82838eee8561e7a908899
aldebaran1 <sebastijan.mrak@gmail.com>
2018-09-17 09:24:45 -0400

    Version for Surav A. JGR paper

--- imaging/example.py
+++ imaging/example.py
@@ -9,17 +9,16 @@ Created on Fri Aug 17 11:06:10 2018
 import cartopy.crs as ccrs
 import os
 import cartomap.geogmap as cm
-import h5py
 import yaml
 import matplotlib.pyplot as plt
 from datetime import datetime
-from numpy import array, multiply, add, ma, rot90, flip, linspace, meshgrid
+from numpy import linspace
 import gps2d
 
-try:
+
+if 'title' in vars():
     del title
-except:
-    pass
+
 # CFG MAP
 wdir = os.getcwd()
 import platform
@@ -40,14 +39,16 @@ elif platform.system() == 'Windows':
 # What to do?
 RGB = 0
 DTEC = 1
-NEXRAD = 1
-TOTALITY = 1
+NEXRAD = 0
+TOTALITY = 0
+T = '2017-8-22T04:30'
 dtype = 'single'
-save = 'E:\\grlfigs\\'
-save = ''
+save = 'E:\\surav\\'
+#save = ''
 
 nqimage = '/nexrad2017-08-20T13-00-00.png'
 nqimage = '/nexrad2017-08-21T18-10-00.png'
+nqimage = '/nexrad2017-08-21T14-00-00.png'
     
 # Map settings
 mapcfg = wdir + '/map/example_map.yaml'
@@ -74,6 +75,12 @@ im_levels = streammap.get('image_nlevels')
 levels = linspace(clim[0],clim[1], im_levels)
 # Image processing
 fillpixeriter = 3
+# Image overlays: Scatter locations
+marker_locations = streammap.get('marker_location')
+marker = streammap.get('marker')
+marker_color = streammap.get('marker_color')
+marker_size = streammap.get('marker_size')
+marker_width = streammap.get('marker_width')
 # --------------------------------------------------------------------------- #
 # Make a map: ALWAYS
 # --------------------------------------------------------------------------- #
@@ -82,7 +89,7 @@ fig = cm.plotCartoMap(projection=projection,
                      meridians=meridians,figsize=figure_size,
                      background_color=background_color,border_color=border_color,
                      grid_color=grid_color,grid_linestyle=grid_linestyle,
-                     grid_linewidth=grid_linewidth, terrain=terrain)
+                     grid_linewidth=grid_linewidth, terrain=terrain,figure=True)
 # --------------------------------------------------------------------------- #
 # Plot the TEC image
 # --------------------------------------------------------------------------- #
@@ -91,6 +98,7 @@ if DTEC:
     datafn = 'single233_02_130_60.h5'
 #    datafn = 'single232_02_130_60.h5'
     #datafn = 'single233_02_cut.h5'
+    datafn = 'single234_02.h5'
     fn = folder + datafn
     #i = 2273 # 18:57
 #    i = 2243 # 18:42
@@ -98,21 +106,26 @@ if DTEC:
 #    i = 2213 # 18:27
 #    i = 1679 # 14:00
 #    i = 1559 # 13:00
-#    i = 1619 # 13:30
-    t, xgrid, ygrid, tec = gps2d.returndTEC(fn, dtype=dtype, darg=i)
+    i = 1619 # 13:30
+    i = 1799
+    t, xgrid, ygrid, tec = gps2d.returndTEC(fn, dtype='t', darg=T)
     if fillpixeriter > 0:
         tec = gps2d.makeImage(tec,pixel_iter=fillpixeriter)
     # Plot data
-    title = 'TEC: ' + str(t[i])
+    if T == '':
+        title = 'TEC: ' + str(t[i])
+    else:
+        title = 'TEC: ' + T
     if image_type == 'contourf':
         tec[tec<=clim[0]] = levels[0]
         tec[tec>=clim[1]] = levels[-1]
-        plt.contourf(xgrid,ygrid,tec.T,levels,cmap=cmap,transform=ccrs.PlateCarree())
+        tidim = plt.contourf(xgrid,ygrid,tec.T,levels,cmap=cmap,transform=ccrs.PlateCarree())
     else:
-        plt.pcolormesh(xgrid,ygrid,tec.T,cmap=cmap,transform=ccrs.PlateCarree())
+        tidim = plt.pcolormesh(xgrid,ygrid,tec.T,cmap=cmap,transform=ccrs.PlateCarree())
     plt.clim(clim)
     # Fig utils
-    cbar = plt.colorbar(ticks=[clim[0], clim[0]/2, 0, clim[1]/2, clim[1]])
+    cbar = plt.colorbar(mappable=tidim, #cax=cax,
+                        ticks=[clim[0], clim[0]/2, 0, clim[1]/2, clim[1]])
     cbar.set_label('$\Delta$TEC [TECu]')
 if NEXRAD:
     if 'title' in vars():
@@ -133,7 +146,15 @@ if NEXRAD:
 if TOTALITY:
     lon_t, lat_t = gps2d.getTotalityCenter(totalityfn)
     plt.plot(lon_t, lat_t-1, '.-k', lw=2, transform=ccrs.PlateCarree())
-    
+
+if marker_locations is not None:
+    if len(marker_locations) > 0 :
+        for loc in marker_locations:
+            plt.scatter(loc[0],loc[1],
+                        marker=marker,s=marker_size,
+                        c=marker_color,lw=marker_width,
+                        transform=ccrs.PlateCarree())
+
 # Make title:
 plt.title(title)
 if save != '':
@@ -141,10 +162,14 @@ if save != '':
         filename = datetime.strftime(t[i], '%Y%d%m-%H%M%S')
         if NEXRAD:
             filename+='_nexrad'
+        if marker_locations is not None:
+            filename += '_markers'
         figname = save + filename + '.png'
-        plt.tight_layout()
         plt.savefig(figname,dpi=300)
         plt.close(fig)
+        print ('Figure saved.')
     except Exception as e:
         print(e)
+else:
+    plt.show()
         
