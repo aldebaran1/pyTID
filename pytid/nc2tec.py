0442da2d0eb9bd743e2b69eb612cc688cba83bf5
aldebaran1 <sebastijan.mrak@gmail.com>
2019-05-31 18:32:31 -0400

    print to log file

--- pytid/nc2tec.py
+++ pytid/nc2tec.py
@@ -18,28 +18,38 @@ import h5py
 from argparse import ArgumentParser
 #import matplotlib.pyplot as plt
 
-OBSFOLDER = '/media/smrak/gnss/obs/'
-NAVFOLDER = '/media/smrak/gnss/nav/'
-SBFOLDER = '/media/smrak/gnss/jplg/'
-SAVEFOLDER = '/media/smrak/gnss/hdf/'
-
 if __name__ == '__main__':
     p = ArgumentParser()
     p.add_argument('date')
-#    p.add_argument('day', type=str)
-    p.add_argument('rxlist', type=str, help='Rxlist as a .yaml file')
-    p.add_argument('--elmask', type=int, default=30)
-    p.add_argument('--tlim', default=None, help="start, stop times example 06:00 08:00", nargs=2, type=str)
-    p.add_argument('-i', '--altkm', type=int, help='Pierce points altitude in kilometers.', default=350)
-    p.add_argument('-o', '--ofn', help='Output filename with or withou root folder. Default destiantion is {}'.format(SAVEFOLDER), default=None)
-    p.add_argument('--ts', help='sampling rate', default=30, type=int)
+    p.add_argument('rxlist', type = str, help = 'Rxlist as a .yaml file')
+    p.add_argument('--elmask', type = int, default = 30)
+    p.add_argument('--tlim', default = None, help = "start, stop times example 06:00 08:00", nargs=2, type=str)
+#    p.add_argument('-i', '--altkm', type = int, help = 'Pierce points altitude in kilometers.', default=350)
+    p.add_argument('-o', '--ofn', help = 'Output filename with or withou root folder.', default=None)
+    p.add_argument('--ts', help = 'sampling rate', default = 30, type = int)
+    p.add_argument('--cfg', help = 'Path to the config (yaml) file', default = None)
+    p.add_argument('--log', help = 'If you prefer to make a .log file?', action = 'store_true')
     P = p.parse_args()
     
+    # GLOBAL VARIABLES
+    if P.cfg is None:
+        OBSFOLDER = '/media/smrak/gnss/obs/'
+        NAVFOLDER = '/media/smrak/gnss/nav/'
+        SBFOLDER = '/media/smrak/gnss/jplg/'
+        SAVEFOLDER = '/media/smrak/gnss/hdf/'
+    else:
+        yamlcfg = yaml.load(open(P.cfg, 'r'))
+        OBSFOLDER = yamlcfg.get('obsfolder')
+        NAVFOLDER = yamlcfg.get('navfolder')
+        SBFOLDER = yamlcfg.get('sbfolder')
+        SAVEFOLDER = yamlcfg.get('savefolder')
+    
+    flog = open()
     date = parser.parse(P.date)
     year = date.year
     day = date.strftime('%j')
     rxlist = os.path.expanduser(P.rxlist)
-    ipp_alt = float(P.altkm)
+#    ipp_alt = float(P.altkm)
     el_mask = P.elmask
     tlim = P.tlim
     Ts = P.ts
@@ -47,7 +57,7 @@ if __name__ == '__main__':
     weights=[1, 4, 7, 10]
     
     # Obs nav
-    if Ts == 1: OBSFOLDER += 'highrate/'
+#    if Ts == 1: OBSFOLDER += 'highrate/'
     nc_root = os.path.join(OBSFOLDER, str(year))
     # Filter input files
     stream = yaml.load(open(rxlist, 'r'))
@@ -67,7 +77,7 @@ if __name__ == '__main__':
     satbias = pyGnss.getSatBias(fjplg)
     # Processing options
     satpos = True
-    ipp = True
+#    ipp = True
     args = ['L1', 'L2']
     #Common time array
     if tlim is None:
@@ -91,10 +101,14 @@ if __name__ == '__main__':
             savefn = P.ofn
         elif os.path.isdir(P.ofn):
             assert os.file.splitext(P.ofn)[1] in ('.h5', '.hdf5')
-            savefn = os.path.join(P.ofn, os.path.split(rxlist)[1] + '_' + str(year) + '_' + str(int(ipp_alt)) +'km.h5')
+            savefn = os.path.join(P.ofn, os.path.split(rxlist)[1] + '_' + str(year) + '.h5')
         else:
             assert os.file.splitext(P.ofn)[1] in ('.h5', '.hdf5')
             savefn = os.path.join(SAVEFOLDER, P.ofn)
+    # Open log file is choosen so
+    if P.log is not None:
+        logfn = os.path.splitext(savefn)[0] + '.log'
+        LOG = open(logfn, 'w')
     # Correct tlim for processing purpuses:
     if P.tlim is not None:
         tlim[0] -= timedelta(hours=1)
@@ -119,7 +133,10 @@ if __name__ == '__main__':
             navdatatime = navdata.time.values
             rxpos[irx] = gr.load(fnc).position_geodetic
             rxn[irx] = nc_rx_name[irx]
-            print ('{}/{}'.format(irx+1, rxl))
+            if P.log is not None:
+                LOG.write('{}/{}\n'.format(irx+1, rxl))
+            else:
+                print ('{}/{}'.format(irx+1, rxl))
             for isv, sv in enumerate(svlist):
                 if isv > 32: 
                     continue
@@ -127,10 +144,9 @@ if __name__ == '__main__':
                     continue
                 
                 try:
-                    D = pyGnss.dataFromNC(fnc,fnav,sv=sv,tlim=tlim,el_mask=el_mask-10, satpos=True, ipp=True, ipp_alt = ipp_alt)
+                    D = pyGnss.dataFromNC(fnc,fnav,sv=sv,tlim=tlim,el_mask=el_mask-10, satpos=True)#, ipp=True, ipp_alt = ipp_alt)
                     idel = D['idel'].values
                     sb = satbias[sv]
-                    
                     
                     dt = D.time.values
                     tsps = np.diff(dt.astype('datetime64[s]'))[0].astype(int)
@@ -164,7 +180,7 @@ if __name__ == '__main__':
                             stec[r[0]:r[-1]] = pyGnss.slantTEC(C1[r[0]:r[-1]], C2[r[0]:r[-1]], 
                                                           L1[r[0]:r[-1]], L2[r[0]:r[-1]])
                     stec += sb
-                    F = pyGnss.getMappingFunction(elv, ipp_alt)
+                    F = pyGnss.getMappingFunction(elv, 350)
                     tec = stec * F
                     tecd = gu.getPlainResidual(tec, Ts=tsps, typ='none', 
                                                maxjump=5, weights=[1,4,7,10])
@@ -179,25 +195,36 @@ if __name__ == '__main__':
                     el[idt, isv, irx] = D.el.values[ixmask][idt_reverse]
                     az[idt, isv, irx] = D.az.values[ixmask][idt_reverse]
                 except Exception as e:
-                    print (e)
+                    if P.log is not None:
+                        LOG.write(e + '\n')
+                    else:
+                        print (e)
         except Exception as e:
-            print (e)
+            if P.log is not None:
+                LOG.write(e + '\n')
+            else:
+                print (e)
     th5 = gu.datetime2posix(t.astype(datetime))
     
     # Dealing with duplicate file names
-    def _sfnadd(f, n):
-        h,t = os.path.splitext(f)
-        return h + '_' + str(n) + t
     if os.path.exists(savefn):
-        c = 1
-        while not os.path.exists(savefn):
-            savefn = _sfnadd(savefn, c)
+        head = os.path.splitext(savefn)[0]
+        c = 0
+        while os.path.exists(savefn):
+            try:
+                c = int(os.path.splitext(savefn)[0].split('_')[-1])
+                c += 1
+            except:
+                c += 1
+            savefn = head + '_' + str(c) + '.h5'
             
     # putting the output file togather
-    print ('Saving data...... {}'.format(os.path.split(savefn)[1]))
+    if P.log is not None:
+        LOG.write('Saving data...... {}\n'.format(os.path.split(savefn)[1]))
+    else:
+        print ('Saving data...... {}'.format(os.path.split(savefn)[1]))
     
     h5file = h5py.File(savefn, 'w')
-    
     h5file.create_dataset('obstimes', data=th5)
     h5file.create_dataset('res', data=residuals, compression='gzip', compression_opts=9)
     if Ts > 10:
@@ -205,7 +232,6 @@ if __name__ == '__main__':
     h5file.create_dataset('snr', data=snr, compression='gzip', compression_opts=9)
     h5file.create_dataset('az', data=az, compression='gzip', compression_opts=9)
     h5file.create_dataset('el', data=el, compression='gzip', compression_opts=9)
-    
     h5file.create_dataset('rx_positions', data=rxpos, compression='gzip', compression_opts=9)
     
     timestamp = datetime.now()
@@ -215,5 +241,7 @@ if __name__ == '__main__':
     h5file.attrs[u'weights'] = weights
     
     h5file.close()
-    
-    print ('{} successfully saved.'.format(savefn))
+    if P.log is not None:
+        LOG.write('{} successfully saved.\n'.format(savefn))
+    else:
+        print ('{} successfully saved.'.format(savefn))
